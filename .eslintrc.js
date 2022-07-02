2881442e4a6c607df125be95d48e9d25467b0569
Tomoroki <jorgechiva98@gmail.com>
2022-07-01 21:29:32 -0500

    Inicio

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        "standard"
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module"
+    },
+    rules: {
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        indent: ["error", 4],
+        "space-before-function-paren": ["error", {
+            anonymous: "always",
+            named: "never",
+            asyncArrow: "always"
+        }]
+    }
+};
